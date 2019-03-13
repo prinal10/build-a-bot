d2026b0f363a46b245a73b3f1b0158901ba040cd
psp58@njit.edu <psp58@njit.edu>
2019-03-13 17:41:25 -0600

    adding nested (child) routes.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Router from 'vue-router';
 import HomePage from '@/home/HomePage';
 import RobotBuilder from '@/build/RobotBuilder';
 import PartInfo from '@/parts/PartInfo';
+import BrowseParts from '@/parts/BrowseParts';
+import RobotHeads from '@/parts/RobotHeads';
+import RobotArms from '@/parts/RobotArms';
+import RobotBases from '@/parts/RobotBases';
+import RobotTorsos from '@/parts/RobotTorsos';
 
 
 Vue.use(Router);
@@ -18,7 +23,34 @@ export default new Router({
       path: '/build',
       name: 'Build',
       component: RobotBuilder
-    }, {
+    },
+    {
+      path: '/parts/browse',
+      name: 'BrowseParts',
+      component: BrowseParts,
+      children: [{
+        name: 'BrowseHeads',
+        path: 'heads',
+        component: RobotHeads
+      },
+        {
+          name: 'BrowseArms',
+          path: 'arms',
+          component: RobotArms
+        },
+        {
+          name: 'BrowseBases',
+          path: 'bases',
+          component: RobotBases
+        },
+        {
+          name: 'BrowseTorsos',
+          path: 'torsos',
+          component: RobotTorsos
+        }
+      ],
+    },
+    {
       path: '/parts/:partType/:id',
       name: 'Parts',
       component: PartInfo,
